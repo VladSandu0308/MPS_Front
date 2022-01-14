052e6ba193cf7a2778d9c92dfe19c242f4e9ec24
Vlad <vlad.sandu99@yahoo.com>
2022-01-15 00:57:31 +0200

    Login&Register

---
+++ src/components/Login.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+async function loginUser(credentials) {
+  return fetch('http://localhost:3000/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(credentials)
+  })
+    .then(data => data.json())
+ }
+
+const Login = () => {
+  const [username, setUserName] = useState();
+  const [password, setPassword] = useState();
+  const [token, setToken] = useState(0);
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const retBody = await loginUser({
+      username,
+      password
+    });
+    console.log(retBody.token);
+    setToken(retBody.token);
+
+  }
+    return (
+      
+      <>
+      {token != 0 ? (
+        <></>
+      ) : (
+        <div className="card shadow mb-4 text-center" style={{ width: '22rem', maxHeight: '40rem', margin: '0 0', backgroundColor: 'gold' }}>
+          <div className="card-body">
+              <h4 className="card-title mb-0 border-bottom font-weight-bold"> Login</h4>
+          </div>
+                  
+          <div className="card-body text-center">
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label for="exampleInputEmail1">Username</label>
+              <input type="text" className="form-control" placeholder="Enter username" onChange={e => setUserName(e.target.value)}/>
+              
+            </div>
+            <div className="form-group">
+              <label for="exampleInputPassword1">Password</label>
+              <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" onChange={e => setPassword(e.target.value)}/>
+            </div>
+            
+            <button type="submit" className="btn btn-dark">Submit</button>
+          </form>
+
+            
+          </div>
+          <div className="card-footer">
+            <small className="text-muted">
+                Don't have an account?
+                <a className="ml-2" href="/register">
+                    Register
+                </a>
+            </small>    
+          </div>
+        </div>
+      )}
+        
+      </>
+      
+    );
+  }
+
+export default Login;
