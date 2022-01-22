c127fb15945554947bb2c743e0c4df88c10b0bdd
Vlad <vlad.sandu99@yahoo.com>
2022-01-22 02:06:10 +0200

    Join and Exit room

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,5 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, Navigate } from 'react-router-dom';
+
+async function postRequest(credentials) {
+  return fetch('http://localhost:3000/exitRoom', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(credentials)
+  })
+    .then(data => data.json())
+ }
 
 const Room = () => {
 
@@ -10,6 +21,26 @@ const Room = () => {
   const [rooms, setRooms] = useState({});
   const [loaded, setLoaded] = useState(0);
   const [ fetchingUser, setFetchingUser ] = useState(true);
+
+  const [created, setCreated] = useState(0);
+
+  const handleLeaveGame = async e => {
+    
+    e.preventDefault();
+    console.log("Room name: " + state.user);
+    const retBody = await postRequest({
+      user_id: state.user,
+      room_id: rooms?.room_id
+    });
+
+    console.log("Error:" + JSON.stringify(retBody));
+
+    if (retBody.message.startsWith("The user has been successfully") ) {
+      console.log("Error:" + JSON.stringify(retBody));
+      setCreated(1); 
+
+    }
+  }
 
   useEffect(() => {
     setLoaded(0);
@@ -55,7 +86,13 @@ const Room = () => {
       <></>
     )
   }
-  console.log(rooms?.admin_name);
+  
+  if (created) {
+    return (
+      <Navigate to="/"/>
+    )
+  }
+
   return (
     <div className="card mb-2 mx-auto" style ={{backgroundColor: 'gold', width: '40rem', top: '13rem'}}>
       <h3 class="card-title border-bottom border-dark font-weight-bold" style ={{marginBottom:'20px'}}> Room: {rooms.room_name} </h3>
@@ -90,12 +127,12 @@ const Room = () => {
             state.user === rooms?.admin_id ? (
               <div className='col-md-12 d-flex justify-content-center gap-3'>
 
-                <button type="button" class="btn btn-dark btn-lg">Start game</button>
+                <button type="button" class="btn btn-dark btn-lg"></button>
                 <button type="button" class="btn btn-dark btn-lg">Setings</button>
-                <button type="button" class="btn btn-dark btn-lg">Leave Game</button>
+                <button type="button" class="btn btn-dark btn-lg" onClick={handleLeaveGame}>Leave Game</button>
               </div>
             ) : (
-              <button type="button" class="btn btn-dark btn-lg">Leave Game</button>
+              <button type="button" class="btn btn-dark btn-lg" onClick={handleLeaveGame}>Leave Game</button>
             )
             
           }
--- src/components/existingGames.js
+++ src/components/existingGames.js
@@ -1,11 +1,49 @@
 import React, { useState, useEffect } from 'react';
+import { Navigate } from 'react-router-dom';
 import {format} from 'react-string-format'
 
-const ExistingGames = () => {
+async function postRequest(credentials) {
+  return fetch('http://localhost:3000/chooseRoom', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(credentials)
+  })
+    .then(data => data.json())
+ }
+
+const ExistingGames = ({token, user}) => {
 
   const [rooms, setRooms] = useState([]);
   const [loaded, setLoaded] = useState(0);
   const [ fetchingUser, setFetchingUser ] = useState(true);
+
+  const [password, setPassword] = useState();
+  const [created, setCreated] = useState(0);
+
+  const handleJoinGame = async (room_name, type) => {
+    
+    console.log("Room name: " + user);
+    const retBody = await postRequest({
+      room_name,
+      type,
+      user_id: user,
+      password
+    });
+
+    console.log("Error:" + JSON.stringify(retBody));
+
+    if (retBody.message.startsWith("The user has been successfully") ) {
+      console.log("Error:" + JSON.stringify(retBody));
+      setCreated(retBody.room_id); 
+
+    }
+
+  console.log("cccc" + created);
+
+
+  }
 
   useEffect(() => {
     setLoaded(0);
@@ -41,6 +79,18 @@ const ExistingGames = () => {
     )
   }
 
+  if(!token) {
+    return (
+      <Navigate to="/login"/>
+    )
+  }
+
+  if (created) {
+    return (
+      <Navigate to={format('/rooms/{0}', created)} state={{ "room_id":created, user }}/>
+    )
+  }
+
   return (    
     <div className="card mb-2 mx-auto" style ={{backgroundColor: 'gold', width: '30rem', top: '5rem'}}>
       <div className='card-body text-center'>
@@ -73,7 +123,15 @@ const ExistingGames = () => {
                 </button>
                 <div id={item.room_id} class="accordion-collapse collapse bg-dark text-white" aria-labelledby="flush-headingOne" data-parent="#accordionFlushExample">
                   <div className='accordion-body'>
-                    <p>{item.room_name}</p>
+                    { item.type === "PRIVATE" ? (
+                      <div className="form-group mb-3">
+                        <label>Password</label>
+                        <input type="password" className="form-control text-white bg-dark" placeholder="Password" onChange={e => setPassword(e.target.value)}/>
+                      </div>
+                    ) : (
+                      <></>
+                    )}
+                    <button type="button" className="btn btn-warning btn-lg font-weight-bold" onClick={() => handleJoinGame(item.room_name, item.type)} >Join Room</button>
                   </div>
                 </div>
                 </h2>
